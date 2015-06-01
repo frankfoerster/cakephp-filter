30d7b0552cf1ce82574defdee7eb1f08a239057d
Stephan Gonder <stephan.gonder@sandstein.de>
2015-06-01 10:51:48 +0200

    fix typos

--- src/Model/Table/FiltersTable.php
+++ src/Model/Table/FiltersTable.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Copyright (c) Frank F�rster (http://frankfoerster.com)
+ * Copyright (c) Frank Förster (http://frankfoerster.com)
  *
  * Licensed under The MIT License
  * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright (c) Frank F�rster (http://frankfoerster.com)
+ * @copyright     Copyright (c) Frank Förster (http://frankfoerster.com)
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 namespace FrankFoerster\Filter\Model\Table;
