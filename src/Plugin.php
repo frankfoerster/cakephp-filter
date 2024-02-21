26d10a331a30bf20d1f4132acc6fe97c48cb181e
frankfoerster <github@frankfoerster.com>
2024-02-21 11:04:20 +0100

    fix: add Plugin.php

---
+++ src/Plugin.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace FrankFoerster\Filter;
+
+use Cake\Core\BasePlugin;
+
+class Plugin extends BasePlugin
+{
+    public function getName(): string
+    {
+        return 'Filter';
+    }
+}
